0b0078d700f68ac0387db6eb5758e24f7f01e732
CharmingBeetle <nina.e.thomas@gmail.com>
2025-08-24 12:55:26 +0100

    feat: calendar component

---
+++ my-app/src/Calendar.js
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+import BpkCalendar, {
+  CALENDAR_SELECTION_TYPE,
+} from '@skyscanner/backpack-web/bpk-component-calendar';
+import BpkInput, {
+  INPUT_TYPES,
+} from '@skyscanner/backpack-web/bpk-component-input';
+import BpkText from '@skyscanner/backpack-web/bpk-component-text';
+import { cssModules } from '@skyscanner/backpack-web/bpk-react-utils';
+import format from 'date-fns/format';
+
+import STYLES from './App.scss';
+
+const getClassName = cssModules(STYLES);
+
+const formatDateFull = (date) => format(date, 'EEEE, do MMMM yyyy');
+const formatMonth = (date) => format(date, 'MMMM yyyy');
+const daysOfWeek = [
+  {
+    name: 'Sunday',
+    nameAbbr: 'Sun',
+    index: 0,
+    isWeekend: true,
+  },
+  {
+    name: 'Monday',
+    nameAbbr: 'Mon',
+    index: 1,
+    isWeekend: false,
+  },
+  {
+    name: 'Tuesday',
+    nameAbbr: 'Tue',
+    index: 2,
+    isWeekend: false,
+  },
+  {
+    name: 'Wednesday',
+    nameAbbr: 'Wed',
+    index: 3,
+    isWeekend: false,
+  },
+  {
+    name: 'Thursday',
+    nameAbbr: 'Thu',
+    index: 4,
+    isWeekend: false,
+  },
+  {
+    name: 'Friday',
+    nameAbbr: 'Fri',
+    index: 5,
+    isWeekend: false,
+  },
+  {
+    name: 'Saturday',
+    nameAbbr: 'Sat',
+    index: 6,
+    isWeekend: true,
+  },
+];
+
+const Calendar = () => {
+  const [selectionConfiguration, setSelectionConfiguration] = useState({
+    type: CALENDAR_SELECTION_TYPE.single,
+    date: null,
+  });
+
+  const handleDateSelect = (date) => {
+    setSelectionConfiguration({
+      type: CALENDAR_SELECTION_TYPE.single,
+      date: date,
+    });
+  };
+
+  return (
+    <div className={getClassName('Calendar')}>
+      <BpkText tagName="h2" textStyle="lg" className={getClassName('Calendar__title')}>
+        Select Your Travel Date
+      </BpkText>
+      <BpkInput
+        id="dateInput"
+        type={INPUT_TYPES.text}
+        name="date"
+        value={(selectionConfiguration.date || '').toString()}
+        placeholder="Departure date"
+        className={getClassName('Calendar__input')}
+      />
+      <BpkCalendar
+        id="calendar"
+        onDateSelect={handleDateSelect}
+        formatMonth={formatMonth}
+        formatDateFull={formatDateFull}
+        daysOfWeek={daysOfWeek}
+        weekStartsOn={1}
+        changeMonthLabel="Change month"
+        nextMonthLabel="Next month"
+        previousMonthLabel="Previous month"
+        selectionConfiguration={selectionConfiguration}
+        className={getClassName('Calendar__component')}
+      />
+    </div>
+  );
+};
+
+export default Calendar;
